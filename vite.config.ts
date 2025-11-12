b7dcc7c9c1282e4eefc88b8f267f811756109d19
unknown <kaiwen.chu58@gmail.com>
2025-11-12 10:49:00 -0800

    again

--- vite.config.ts
+++ vite.config.ts
@@ -33,6 +33,15 @@ function obfuscatePlugin(): Plugin {
 
 export default defineConfig({
   base: './',
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
+      },
+    },
+  },
   plugins: [
     react(),
     obfuscatePlugin(),
