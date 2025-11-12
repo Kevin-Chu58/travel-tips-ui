4784b599477382eb681721e68a274a19cbe24c1d
unknown <kaiwen.chu58@gmail.com>
2025-11-12 10:31:12 -0800

    switch from browser router to hash router

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
 import { StrictMode, type JSX } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, HashRouter } from "react-router-dom";
 import { Auth0Provider, type AppState } from "@auth0/auth0-react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { SnackbarProvider } from 'notistack';
-import "./index.scss";
-import App from "./App.tsx";
 import { ThemeProvider } from "@mui/material";
 import theme from "./theme.tsx";
 import { Provider } from "react-redux";
 import { store } from "@redux/store";
+import App from "./App.tsx";
+import "./index.scss";
 
 let domain = import.meta.env.VITE_AUTH0_DOMAIN;
 let clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
@@ -41,13 +41,15 @@ const Auth0Layer = (): JSX.Element => {
     >
       <Provider store={store}>
         <QueryClientProvider client={queryClient}>
-          <Router>
+          {/* <Router> */}
+          <HashRouter>
             <SnackbarProvider maxSnack={3}>
             <ThemeProvider theme={theme}>
               <App />
             </ThemeProvider>
             </SnackbarProvider>
-          </Router>
+          </HashRouter>
+          {/* </Router> */}
         </QueryClientProvider>
       </Provider>
     </Auth0Provider>
