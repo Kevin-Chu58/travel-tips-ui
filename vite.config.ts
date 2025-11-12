0dac4b4132fd17e977b217fea076d7cd7dc2c0f9
unknown <kaiwen.chu58@gmail.com>
2025-11-12 09:35:13 -0800

    vite.config fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import JavaScriptObfuscator from 'javascript-obfuscator';
 import type { Plugin } from 'vite';
 import type { OutputBundle, OutputChunk } from 'rollup';
 
-// Custom plugin to obfuscate JS after build
+// Obfuscate JS after build
 function obfuscatePlugin(): Plugin {
   return {
     name: 'obfuscate-plugin',
@@ -14,7 +14,7 @@ function obfuscatePlugin(): Plugin {
     generateBundle(_, bundle: OutputBundle) {
       for (const [_, file] of Object.entries(bundle)) {
         if (file.type === 'chunk') {
-          const chunk = file as OutputChunk
+          const chunk = file as OutputChunk;
           const obfuscationResult = JavaScriptObfuscator.obfuscate(chunk.code, {
             compact: true,
             controlFlowFlattening: true,
@@ -22,22 +22,22 @@ function obfuscatePlugin(): Plugin {
             stringArray: true,
             rotateStringArray: true,
             stringArrayEncoding: ['base64'],
-            identifierNamesGenerator: 'mangled', // <- Short variable names
-          })
-          chunk.code = obfuscationResult.getObfuscatedCode()
+            identifierNamesGenerator: 'mangled',
+          });
+          chunk.code = obfuscationResult.getObfuscatedCode();
         }
       }
     },
-  }
+  };
 }
 
 export default defineConfig({
-  base: './',
+  base: '/', // <- important for Azure deployment
   plugins: [
     react(),
     obfuscatePlugin(),
     tsconfigPaths({
-      projects: ['./tsconfig.app.json']  // explicitly tell it to use this
+      projects: ['./tsconfig.app.json'],
     }),
-  ]
+  ],
 });
