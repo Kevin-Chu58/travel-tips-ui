38f2ce9c4609e903f8418d3d92fa6ebc2328d072
unknown <kaiwen.chu58@gmail.com>
2026-05-09 14:09:53 -0700

    try fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,37 +1,37 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
-import JavaScriptObfuscator from "javascript-obfuscator";
-import type { Plugin } from "vite";
-import type { OutputBundle, OutputChunk } from "rollup";
+// import JavaScriptObfuscator from "javascript-obfuscator";
+// import type { Plugin } from "vite";
+// import type { OutputBundle, OutputChunk } from "rollup";
 import compression from "vite-plugin-compression";
 
 // Custom plugin to obfuscate JS after build
-function obfuscatePlugin(): Plugin {
-  return {
-    name: "obfuscate-plugin",
-    apply: "build",
-    enforce: "post",
+// function obfuscatePlugin(): Plugin {
+//   return {
+//     name: "obfuscate-plugin",
+//     apply: "build",
+//     enforce: "post",
 
-    generateBundle(_, bundle: OutputBundle) {
-      for (const [_, file] of Object.entries(bundle)) {
-        if (file.type === "chunk") {
-          const chunk = file as OutputChunk;
-          const obfuscationResult = JavaScriptObfuscator.obfuscate(chunk.code, {
-            compact: true,
-            // controlFlowFlattening: true,
-            // deadCodeInjection: true,
-            stringArray: true,
-            rotateStringArray: true,
-            stringArrayEncoding: ["base64"],
-            identifierNamesGenerator: "mangled", // <- Short variable names
-          });
-          chunk.code = obfuscationResult.getObfuscatedCode();
-        }
-      }
-    },
-  };
-}
+//     generateBundle(_, bundle: OutputBundle) {
+//       for (const [_, file] of Object.entries(bundle)) {
+//         if (file.type === "chunk") {
+//           const chunk = file as OutputChunk;
+//           const obfuscationResult = JavaScriptObfuscator.obfuscate(chunk.code, {
+//             compact: true,
+//             // controlFlowFlattening: true,
+//             // deadCodeInjection: true,
+//             stringArray: true,
+//             rotateStringArray: true,
+//             stringArrayEncoding: ["base64"],
+//             identifierNamesGenerator: "mangled", // <- Short variable names
+//           });
+//           chunk.code = obfuscationResult.getObfuscatedCode();
+//         }
+//       }
+//     },
+//   };
+// }
 
 export default defineConfig({
   base: "/",
@@ -64,7 +64,7 @@ export default defineConfig({
   },
   plugins: [
     react(),
-    obfuscatePlugin(),
+    // obfuscatePlugin(),
     tsconfigPaths({
       projects: ["./tsconfig.app.json"], // explicitly tell it to use this
     }),
