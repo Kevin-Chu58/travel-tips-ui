6119f29f8b3282dd9a4f1252628204a84c9c5d14
unknown <kaiwen.chu58@gmail.com>
2026-05-12 18:03:12 -0700

    update default ad cards link

--- src/constants/Defaults.ts
+++ src/constants/Defaults.ts
@@ -9,7 +9,7 @@ export const ads = [
     imageId: 46,
     text: "Make your destination private.",
     linkLabel: "Join now!",
-    link: "https://www.google.com",
+    link: "https://traveltipsgo.com/membership",
     picture:
       "https://firebasestorage.googleapis.com/v0/b/traveltips-imagestore.firebasestorage.app/o/assets%2FDefaultAds%2Fdefault_ads_1.jpeg?alt=media&token=3db4b4f0-80c6-4de5-8f39-0c89ac2cd779",
     subStatus: "active",
@@ -25,7 +25,7 @@ export const ads = [
     imageId: 47,
     text: "Promote your business",
     linkLabel: "Join Now",
-    link: "https://www.google.com",
+    link: "https://traveltipsgo.com/partnership",
     picture:
       "https://firebasestorage.googleapis.com/v0/b/traveltips-imagestore.firebasestorage.app/o/assets%2FDefaultAds%2Fdefault_ads_2.jpeg?alt=media&token=9ec1b2a8-40a9-42d1-8415-29c8451ee85b",
     subStatus: "active",
